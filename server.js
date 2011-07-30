6c93cd5cb4cc6417cebbe8e6787089ae9a0a67d4
Lorenzo Urbini <lorenzo.urbini@edgeint.net>
2011-07-30 17:23:14 +0100

    changed application scope with simple useragent and only click to community

--- server.js
+++ server.js
@@ -3,28 +3,28 @@ var sys = require('sys'),
     fs = require('fs'),
     url = require('url'),
     path  = require("path"),
-    fourzerofour, orderpage; 
+    fourzerofour, orderpage;
     
-fs.readFile('./index.html', function (err, data) {
+fs.readFile('./mobilehome.html', function (err, data) {
     if (err) {
         throw err;
     }
-    index = data;
+    mobilehome = data;
+}); 
+    
+fs.readFile('./home.html', function (err, data) {
+    if (err) {
+        throw err;
+    }
+    home = data;
 });
          
-fs.readFile('./location1.html', function (err, data) {
+/*fs.readFile('./location1.html', function (err, data) {
     if (err) {
         throw err;
     }
     location1 = data;
-});
-
-fs.readFile('./location2.html', function (err, data) {
-    if (err) {
-        throw err;
-    }
-    location2 = data;
-});
+});*/
 
 
 fs.readFile('./404.html', function (err, data) {
@@ -42,34 +42,57 @@ fs.readFile('./404.html', function (err, data) {
   console.log("Server has started.");
 
   function onRequest(request, response) {
+  	
+  		var deviceIphone = "iphone";
+  		var deviceIpad = "ipad";
+  		var deviceAndroid = "android";
+  		var deviceXoom = "xoom";
+  		var deviceHtcFlyer = "htc_flyer";
+  		var deviceKindle = "kindle"; 
+  		var deviceBB = "blackberry";
+  		var devicePalm = "palm";
+		var deviceWebOS = "webos"; //For Palm's line of WebOS devices
+		var deviceWebOShp = "hpwos"; //For HP's line of WebOS devices
+		var deviceTablet = "tablet";
+
+  		var useragent = request.headers['user-agent'].toLowerCase();
+
         var pathname = url.parse(request.url).pathname;
-        if((pathname.match(/\d{1,2}$/)) || (pathname.match(/^\/$/)) ){
-            if (pathname.match(/^\/$/)) {
-            returnHome(response);
+            if (pathname.match(/^\/$/)) 
+            {            	  		
+	  		 	if ((useragent.search(deviceIphone) > -1) || (useragent.search(deviceAndroid) > -1)) 
+	  		 	{ 
+	  		 		returnMobileHome(response);
+	  		 	}
+	  		 	else 
+	  		 	{ 
+	  		 		returnHome(response); 
+	  		 	}
             }
-            else if(request.url.match(/\d{1,2}$/)) 
+            /*else if(request.url.match(/\d{1,2}$/)) 
             {
                 address = request.url;
                 address = address.match(/\d{1,2}$/);
-
-                 returnLocationPage(address, response);                
-            }
-            else if(request.url == "7guerrilla_o.jpg")
-            {
-            	response.writeHead(404, {"Content-Type": "text/html"});
-        	    response.write(fourzerofour);
-        	} 
+				returnLocationPage(address, response);                
+            }*/
             
-        } else {
-        response.writeHead(404, {"Content-Type": "text/html"});
-        response.write(fourzerofour);
+            else 
+            {
+		        response.writeHead(404, {"Content-Type": "text/html"});
+		        response.write(fourzerofour);
+		        response.end();
+       		 }
+  }
+  
+  function returnMobileHome(response){      
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(mobilehome);
         response.end();
-        }
   }
   
   function returnHome(response){      
         response.writeHead(200, {"Content-Type": "text/html"});
-        response.write(index);
+        response.write(home);
         response.end();
   }
   
